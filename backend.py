c31fe73a102eba12a806f3cfb5efbd7a51b8341e
KJ <james.t.butcher@icloud.com>
2021-02-08 23:18:02 +0000

    Version 1.0

--- backend.py
+++ backend.py
@@ -5,7 +5,7 @@ import time
 
 
 
-class discordrpc():
+class discordrpc:
     def __init__(self):
         self.activeProcess = []
 
@@ -66,6 +66,10 @@ class discordrpc():
         self.activeProcess.append(True)
         self.activeProcess[-1] = mp.Process(target=self._stayConnected, daemon=True)
 
+<<<<<<< HEAD
+        #self.process.daemon = True
+=======
+>>>>>>> e758b9501896bd9a5ca10c298418f6e8148fd7df
         self.activeProcess[-1].start()
 
         return
